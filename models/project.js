26e2a020498844633f7d91db9809a010a14c9b69
laheravilla <laheravilla@gmail.com>
2019-09-05 05:11:25 +0200

    How to create a Model and its schemato export it to database

---
+++ models/project.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema; // Definir esquema del modelo
+
+var ProjectSchema = Schema({
+    name: String,
+    lastname: String,
+    description: String,
+    category: String,
+    year: Number,
+    technologies: [String]
+});
+
+module.exports = mongoose.model('Project', ProjectSchema); // Mongoose export project name in plural and lowercase (projects)
