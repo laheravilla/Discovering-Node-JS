0d4a88d8e07282adec9c5bae1906d125a14f240a
laheravilla <laheravilla@gmail.com>
2019-09-10 03:39:40 +0200

    Setting headers & cors for ajax request

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json()); // Convert POST files in json
 
 // CORS
+// Setting headers & cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 
 // Routes
